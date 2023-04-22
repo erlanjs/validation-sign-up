c5cd036d968ba2a043cf4c862be3fccdb0829a06
Erlan Zhusuppov <erlan@mancho.dev>
2023-04-22 20:45:23 +0600

    Домашная задание

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h4>Авторизация</h4>
+      </div>
+      <div className="forms">
+        <input type="text" placeholder="Имя пользовотелья" />
+        <input type="email" placeholder="Адрес электронный почты" />
+        <input type="text" placeholder="Придумаете пароль" />
+        <input type="text" placeholder="Повторите пароль" />
+      </div>
+
+      <button className="submit">Войти</button>
     </div>
   );
 }
